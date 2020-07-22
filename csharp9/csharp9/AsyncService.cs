522f670f4abd4f8923af5bb3262479c4233d6c95
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-07-22 11:53:16 -0400

    add Top-Level programs sample

---
+++ csharp9/csharp9/AsyncService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace csharp9
+{
+    public class AsyncService
+    {
+        public async Task<string> MyAsyncMethod() => "test";
+    }
+}
---
+++ csharp9/csharp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using csharp9;
+using System;
+
+// Before
+//namespace csharp9
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+// Now
+var s = new AsyncService();
+var result = await s.MyAsyncMethod();
+Console.WriteLine($"Hello {args[0]}! result: {result}");
+return 1;
