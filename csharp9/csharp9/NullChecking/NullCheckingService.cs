5fb6b8e3be071d3b4b317c5c61cca2f57b05d05b
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-08-19 15:39:12 -0400

    null checking

---
+++ csharp9/csharp9/NullChecking/NullCheckingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp9.NullChecking
+{
+    class NullCheckingService
+    {
+        public void Before(string name)
+        {
+            if (name is null)
+                throw new ArgumentNullException();
+        }
+
+        // Not supported in VS yet
+        //public void Now(string name!)
+        //{
+        //}
+    }
+}
