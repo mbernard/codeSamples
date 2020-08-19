ac5f83a5c7f5fce636a431cb030a41a94f076cb9
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-08-19 09:19:23 -0400

    Add targetTyping

---
+++ csharp9/csharp9.Tests.Unit/TargetType/TargetTypeTests.cs
@@ -0,0 +1,19 @@
+﻿using csharp9.TargetTyped;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace csharp9.Tests.Unit.TargetType
+{
+    public class TargetTypeTests
+    {
+        [Fact]
+        public void NewTargetType()
+        {
+            Cat c = new("Chester"); // C# 9.0
+        }
+    }
+}
---
+++ csharp9/csharp9/TargetTyped/Types.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp9.TargetTyped
+{
+    public abstract class Animal
+    {
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string breed)
+        {
+            Breed = breed;
+        }
+
+        public string Breed { get; }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
