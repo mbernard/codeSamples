1703a5a63f7091ccac500fdd1dbc7fda69dda6e4
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-10-12 17:30:54 -0400

    Partial method extensions

---
+++ csharp9/csharp9.Tests.Unit/NullParameterCheck/NullParameterCheckTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace csharp9.Tests.Unit.NullParameterCheck
+{
+    public class NullParameterCheckTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+
+        public class NullService
+        {
+            public void SomeMethod(string name!)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ csharp9/csharp9/ExtendingPartialMethods/Parser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp9.ExtendingPartialMethods
+{
+    partial class Parser
+    {
+        internal partial bool TryParse(string s, out int i);
+    }
+
+    partial class Parser
+    {
+        internal partial bool TryParse(string s, out int i)
+        {
+            i = 0;
+            return true;
+        }
+    }
+}
