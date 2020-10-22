7e6b69e2fe6d1771a2c20286bd218b10400f313e
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-10-22 15:10:59 -0400

    Improved pattern maching examples

---
+++ csharp9/csharp9.Tests.Unit/ImprovedPatternMatching/ImprovedPatternMatchingTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace csharp9.Tests.Unit.ImprovedPatternMatching
+{
+    public class ImprovedPatternMatchingTests
+    {
+        [Theory]
+        [InlineData("string")]
+        [InlineData(0)]
+        public void NotKeyword(object s)
+        {
+            if (s is not string) { }
+
+            // is way more easier to read than
+            if (!(s is string)) { }
+        }
+
+        public record Person(int? Weight);
+
+        [Fact]
+        public void ImprovedPatternMatching()
+        {
+            var p = new Person(0);
+            var a = p.Weight switch
+            {
+                < 150 => "light",
+                >= 150 and < 200 => "normal",
+                not null => "unknown",
+                null => "error",
+            };
+        }
+
+    }
+}
