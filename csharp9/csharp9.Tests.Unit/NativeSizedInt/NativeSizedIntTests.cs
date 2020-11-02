9fee488c483a67179a709431fc2c974511539989
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-11-02 11:05:31 -0500

    add native sized int

---
+++ csharp9/csharp9.Tests.Unit/NativeSizedInt/NativeSizedIntTests.cs
@@ -0,0 +1,26 @@
+﻿
+using Xunit;
+
+namespace csharp9.Tests.Unit.NativeSizedInt
+{
+    public class NativeSizedIntTests
+    {
+        [Fact]
+        public void GivenGiven_WhenWhen_ThenThen()
+        {
+            // Given
+            nint i = 2;
+            var p = nint.Parse("123");
+            var p2 = nint.Parse("3");
+
+
+            // When
+            var t = p * i;
+            //var t2 = p * p2; This is not allowed
+
+            // Then
+            Assert.Equal(246, t);
+        }
+
+    }
+}
